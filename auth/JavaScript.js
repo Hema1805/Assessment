6df5a98f3a2a90f0a04fc7b02b7d05d8e712ecb2
Hema1805 <142032239+Hema1805@users.noreply.github.com>
2026-08-15 15:34:11 +0530

    commit

--- auth/JavaScript.js
+++ auth/JavaScript.js
@@ -1,6 +1,74 @@
-const form = document.getElementById("loginForm"); 
+const loginForm = document.getElementById("loginForm");
 
-const username = document.getElementById("username"); 
+if (loginForm)
+{
+    loginForm.addEventListener("submit", function(event) 
+    {
+    event.preventDefault();
+    const username = document.getElementById("username").value.trim();
+    const password = document.getElementById("password").value;
+    const errorMessage = document.getElementById("errorMessage");
+    errorMessage.textContent = "";
 
-form.addEventListener("submit", async (event) => {    event.preventDefault(); });
+    if (username === "")
+    {
+        errorMessage.textContent = "Please enter username.";
+        return;
+    }
 
+    if (password === "")
+    {
+        errorMessage.textContent = "Please enter password.";
+        return;
+    }
+
+    const correctUsername = "Hema";
+    const correctPassword = "12345";
+
+    if (username === correctUsername && password === correctPassword)
+    {
+        localStorage.setItem("isLoggedIn", "true");
+
+        localStorage.setItem("username", username);
+
+        window.location.href = "dashboard.html";
+
+    } 
+    else
+    {
+        errorMessage.textContent ="Invalid username or password.";
+    }
+});
+}
+
+if (window.location.pathname.includes("dashboard.html")) 
+{
+    const isLoggedIn = localStorage.getItem("isLoggedIn");
+
+    if (isLoggedIn !== "true")
+    {
+        alert("Please login first.");
+        window.location.href ="index.html";
+    }
+
+    const username = localStorage.getItem("username");
+    const welcomeMessage =
+    document.getElementById("welcomeMessage");
+
+    if (welcomeMessage && username)
+    {
+        welcomeMessage.textContent ="Hello, " + username + "!";
+    }
+}
+
+const logoutButton = document.getElementById("logoutButton");
+
+if (logoutButton) 
+    {
+        logoutButton.addEventListener("click", function()
+        {
+            localStorage.removeItem("isLoggedIn");
+            localStorage.removeItem("username");
+            window.location.href = "index.html";
+        });
+    }
